1522c042bb808c5c34f8b78140660f4cd0b21abd
leo60228 <leo@60228.dev>
2021-04-05 18:17:02 -0400

    init

---
+++ serve.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+import logging
+import asyncio
+import sys
+from aiohttp import web
+
+@web.middleware
+async def default_middleware(request, handler):
+    try:
+        response = await handler(request)
+        if response.status != 404 and response.status != 403:
+            return response
+    except web.HTTPException as ex:
+        if ex.status != 404 and ex.status != 403:
+            raise
+    return web.FileResponse('./index.html')
+
+async def stream_data(request):
+    resp = web.StreamResponse()
+    resp.content_type = 'text/event-stream'
+    resp.charset = 'utf-8'
+
+    writer = await resp.prepare(request)
+
+    with open('./events/streamData', 'rb') as event:
+        await resp.write(event.read())
+        await resp.write(b'\n')
+        await writer.drain()
+
+    await asyncio.sleep(30)
+
+    await resp.write_eof()
+
+    return resp
+
+app = web.Application(middlewares=[default_middleware])
+
+app.add_routes([web.get('/events/streamData', stream_data), web.static('/', '.')])
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    web.run_app(app)
